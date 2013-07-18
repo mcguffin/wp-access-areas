4a91fae7d0115543ea0ef4a9be455ac4b3b41195
mcguffin <joern@flyingletters.com>
2013-07-18 09:57:35 +0200

    changed mkzip.sh to work with trunk dir as well

--- mkzip.sh
+++ mkzip.sh
@@ -1,3 +1,8 @@
 CWDNAME=${PWD##*/}
+
+if [ "$CWDNAME" == "trunk" ]; then
+	parent=${PWD%/*}
+	CWDNAME=${parent##*/}
+fi
 #find /$CWDNAME -path '*/.*' -prune -o -type f -print | zip ../$CWDNAME.zip -@
 find . -path '*/.*' -prune -o -type f \( ! -iname 'README.md'  ! -iname 'mkzip.sh' \) -print | zip ../$CWDNAME.zip -@
