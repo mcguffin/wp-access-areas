0b6c02adcceaecc23654737232b5c7866abb97b9
mcguffin <joern@flyingletters.com>
2018-01-02 11:16:44 +0100

    wpmu uninstaller page

--- include/AccessAreas/Compat/WPMU/NetworkAdmin.php
+++ include/AccessAreas/Compat/WPMU/NetworkAdmin.php
@@ -21,6 +21,7 @@ class NetworkAdmin extends Core\Singleton {
 		if ( current_user_can( 'manage_network_users' ) ) {
 			$admin = \AccessAreas\Admin\AdminPageAccessAreas::instance();
 			add_action( 'network_admin_menu', array( $admin, 'add_admin_page' ));
+			add_action( 'network_admin_menu', array( $this, 'add_uninstaller' ));
 			add_filter( 'access_areas_current_blog_id',array($this,'return_zero_string'));
 
 		}
@@ -32,4 +33,27 @@ class NetworkAdmin extends Core\Singleton {
 		return '0';
 	}
 
+	public function add_uninstaller() {
+		add_settings_page( __( 'Access Areas Uninstall' , 'wp-access-areas' ), __( 'Uninstall Access Areas' , 'wp-access-areas' ), 'manage_network', 'access_areas-uninstall', array( $this, 'render_uninstaller_page' ) );
+		add_settings_page('foo','bar')
+	}
+
+	/**
+	 * 	Add Admin page to menu
+	 */
+	function render_page() {
+		?><div class="wrap">
+			<h2><?php _e( 'Access Areas Uninstaller' , 'wp-access-areas' ); ?></h2>
+			<?php _e('Uninstall Instructions ...', 'wp-access-areas' ); ?>
+			<ol>
+				<?php if ( get_site_option( 'wpaa_uninstall_active' ) ) { ?>
+					<li><?php _e('Enable uninstall mode first', 'wp-access-areas' ); ?></li>
+				<?php } ?>
+				<li><?php printf( __('Visit the %s page', 'wp-access-areas' ), sprintf( '<a href="%s">%s</a>', network_admin_url('upgrade.php'), __('Upgrade Network','wp-access-areas') ); ?></li>
+			</ol>
+			<button><
+		</div><?php
+	}
+
+render_uninstaller_page
 }
